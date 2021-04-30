8e22632fb79ad8f60ff1df6a322241045bffb4db
Jose Ballesteros <josebp0107@gmail.com>
2021-04-30 17:49:43 -0500

    Empece a agregar elementos y estilos al componente Badge

---
+++ src/components/Badge.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './styles/Badges.css';
+//import confLogo from '../images/nombre_de_la_imagen';
+
+class Badge extends React.Component {
+    render() {
+        return (
+            // Es importante saber que, las clases en jsx cambia su nombre a className
+            <div className="Badge">
+                <div className="Badge__header">
+                    <h1>TechConf</h1>
+                     
+                </div>
+
+                <div className="Badge__section-name">
+                    <img className="Badge__avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
+                    <h1>Jose David <br/> Ballesteros</h1>
+                </div>
+
+                <div className="Badge__section-info">
+                    <p>Software Engineer</p>
+                    <p>@jose0107</p>
+                </div>
+
+                <div className="Badge__footer">
+                    #Conf
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default Badge;
+
