deb87a4857ee828f4c90aee01b25d8da33f93a99
Jose Ballesteros <josebp0107@gmail.com>
2021-05-01 11:24:59 -0500

    Termine de agregar el componente de Badge a BadgeNew

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -13,14 +13,14 @@ class Badge extends React.Component {
 
                 <div className="Badge__section-name">
                     <img className="Badge__avatar" 
-                    src="https://www.gravatar.com/avatar?d=identicon" 
+                    src={this.props.avatar}
                     alt="Avatar" />
-                    <h1>Jose David <br/> Ballesteros</h1>
+                    <h1>{this.props.firstName} <br/> {this.props.lastName}</h1>
                 </div>
 
                 <div className="Badge__section-info">
-                    <h3>Software Engineer</h3>
-                    <div>@jose0107</div>
+                    <h3>{this.props.jobTitle}</h3>
+                    <div>@{this.props.twitter}</div>
                 </div>
 
                 <div className="Badge__footer">
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-
+import Badge from '../components/Badge';
 import Navbar from '../components/Navbar';
 
 class BadgeNew extends React.Component {
@@ -8,7 +8,21 @@ class BadgeNew extends React.Component {
         return (
             <div>
                 <Navbar />
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <Badge 
+                                firstName="Jose David"
+                                lastName="Ballesteros" 
+                                josbTitle="Software Engineer"
+                                twitter="jose0701"
+                                avatar="https://www.gravatar.com/avatar?d=identicon"
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
+            
         )
     }
 }
